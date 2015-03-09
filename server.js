7a5e03a60268007cd5bee823cf2aab5f02ac5117
rodrigopavezi <rodrigopavezi@gmail.com>
2015-03-09 15:00:53 -0300

    Raised number of phanthom js running in a cluster to 2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var prerender = require('prerender');
 
 var server = prerender({
-	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 2,
+	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 5,
 	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
 	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
     });
